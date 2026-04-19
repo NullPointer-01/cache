dd7f5adc15dd8b7868eddf2731a9835a80d30e10
nullpointer-01 <223323887+NullPointer-01@users.noreply.github.com>
2026-04-19 07:58:14 +0530

    Overwrite older read events on overflow

--- src/main/java/org/nullpointer/cache/model/StripedBuffer.java
+++ src/main/java/org/nullpointer/cache/model/StripedBuffer.java
@@ -1,32 +1,24 @@
 package org.nullpointer.cache.model;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class StripedBuffer<K> {
-    private static final int MAX_READ_EVENTS = 256;
+    private static final int READ_BUFFER_CAPACITY = 256;
 
-    private final ConcurrentLinkedQueue<K> readBuffer; // Buffer size is bounded
+    private final CircularBuffer<K> readBuffer;
     private final ConcurrentLinkedQueue<Event<K>> writeBuffer; // Unbounded buffer
-    private final AtomicInteger readSize;
 
     public StripedBuffer() {
-        this.readBuffer = new ConcurrentLinkedQueue<>();
+        this.readBuffer = new CircularBuffer<>(READ_BUFFER_CAPACITY);
         this.writeBuffer = new ConcurrentLinkedQueue<>();
-        this.readSize = new AtomicInteger();
     }
 
     public void recordAccess(K key) {
-        // Drop read events if size exceeds. Leads to slight stale eviction
-        if (readSize.get() >= MAX_READ_EVENTS) return;
-        readSize.incrementAndGet();
         readBuffer.offer(key);
     }
 
     public K pollAccess() {
-        K key = readBuffer.poll();
-        if (key != null) readSize.decrementAndGet();
-        return key;
+        return readBuffer.poll();
     }
 
     public void recordAdd(K key) {
